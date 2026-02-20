7a271f5bbb923b5fe7a964af8141c73af838ff91
brijaygits <fortgamer300@gmail.com>
2026-02-20 21:52:19 +0530

    implemented tpying animation effect using Typed.js

--- script.js
+++ script.js
@@ -9,3 +9,12 @@ window.onscroll = () =>{
     menu.classList.remove('bx-x');
     navbar.classList.remove('active');
 }
+
+/*Typing Text Code*/
+    const typed = new Typed('.multiple-text', {
+      strings: ['Physical Fitness', 'Weight Gain','Strength Training','Fat Lose','Weight Lifting','Running'],
+      typeSpeed: 60,
+      backSpeed:60,
+      backDelay:1000,
+      loop: true,
+    });
